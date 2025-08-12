f43fb61a1d7ec83ef49c82503ca32abe398d67d8
Prajwal <prajwalwankhede890@gmail.com>
2025-08-12 23:35:15 +0530

    Save all changes

--- Flask/flask/htmlAdding.py
+++ Flask/flask/htmlAdding.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 # Create a instance of the flask  Class 
 #  which will be  your  WSGI Application 
 app = Flask(__name__)
 @app.route("/")
 def welcome():
-    return "Welcome to this best flask tutorial. this should be a"
+    return "Welcome to this best flask tutorial. this should be a "
 
-@app.route("/lec1")
-def Lec1():
-    return "this is the lec1"
+@app.route("/Home")
+def Home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
