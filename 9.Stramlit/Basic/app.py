e047d66dd1d6a08b98b893536c44e9a8980c9cad
Prajwal <prajwalwankhede890@gmail.com>
2025-08-20 00:27:33 +0530

    streamlit: Add basic app and widgets

--- 9.Stramlit/Basic/app.py
+++ 9.Stramlit/Basic/app.py
@@ -1,5 +1,7 @@
 #import streamlit as st
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 #create a title for the app
 st.title("My First Streamlit App")
@@ -14,5 +16,30 @@ if st.button("submit"):
 
     # run the app
     # streamlit run app.py
+
+
+# slideer
+st.subheader("Slider Example")
+slider_value = st.slider("Select a value", 0, 100, 50)
+st.write(f"Slider value: {slider_value}")
+
+# checkbox
+st.subheader("Checkbox Example")
+checkbox_value = st.checkbox("Check me!")
+if checkbox_value:
+    st.write("Checkbox is checked!")
+
+#  display a line chart
+st.subheader("Line Chart Example")
+
+dataframe= pd.DataFrame({
+    'Column 1': np.random.randn(10),
+    'Column 2': np.random.randn(10),
+    'Column 3': np.random.randn(10)})
+
+st.write("here is dataframe:")
+st.dataframe(dataframe)
+
+st.line_chart(dataframe)
 
 
